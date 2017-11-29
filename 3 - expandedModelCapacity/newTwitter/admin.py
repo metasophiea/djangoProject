f32694da981dc964884fb5f609e3c433d11b46cc
Brandon Walsh <metasophiea@gmail.com>
2017-11-29 10:20:40 +0000

    combined model with model registration from admin

--- 3 - expandedModelCapacity/newTwitter/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-
-class tweet_admin(admin.ModelAdmin):
-	list_display = ['tweetId','author','text','upVotes','downVotes']
-
-admin.site.register(models.tweet.tweet, tweet_admin)
