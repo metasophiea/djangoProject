8250527e81ba3da03345c5ca8b2a21ae0d547382
Brandon <metasophiea@gmail.com>
2017-12-08 10:23:38 +0000

    oops, no need for the static

--- 4 - moreCompleteAuthentication/static/js/code.js
+++
@@ -1,9 +0,0 @@
-function dealWithData(data){
-	for(var a = 0; a < data.length; a++){
-		p = document.createElement("p");
-		p.innerHTML += data[a].text;
-		p.innerHTML += "<br/>";
-		p.innerHTML += "- " + data[a].authorName;
-		document.body.appendChild(p);
-	}
-}
