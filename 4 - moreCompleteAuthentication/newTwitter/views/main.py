b53be4ebd2c4fac4a49a80f9997c856d87a889d1
Brandon <metasophiea@gmail.com>
2017-12-12 16:38:43 +0000

    signup page

--- 4 - moreCompleteAuthentication/newTwitter/views/main.py
+++ 4 - moreCompleteAuthentication/newTwitter/views/main.py
@@ -16,6 +16,11 @@ def profile(request):
 	context = {}
 	return django.http.HttpResponse(template.render(context, request))
 
+def signinup(request):
+	template = django.template.loader.get_template('newTwitter/signinup.html')
+	context = {}
+	return django.http.HttpResponse(template.render(context, request))
+
 @login_required
 def privateStuff(request):
 	print("Luckily, the user " + str(request.user) + " is logged in")
--- 4 - moreCompleteAuthentication/website/urls.py
+++ 4 - moreCompleteAuthentication/website/urls.py
@@ -2,13 +2,29 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from newTwitter import views
 from django.views.generic.base import TemplateView
+import allauth.account.views
+import django
 
 import newCode
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
 
+	url(r'^signup/$', lambda request: django.http.HttpResponseRedirect("/accounts/signup"), name='signup'),
+	url(r'^login/$', lambda request: django.http.HttpResponseRedirect("/accounts/login"), name='login'),
+	url(r'^logout/$', lambda request: django.http.HttpResponseRedirect("/accounts/logout"), name='logout'),
+
     url(r'^accounts/', include('allauth.urls')),
+	# /signup
+	# /login
+	# /logout
+	# /password/change
+	# /password/set
+	# /inactive
+	# /email
+	# /confirm-email
+	# /password/reset/done/
+	# /password/reset/key/done
 
 	url(r'^privateStuff/$', views.main.privateStuff),
 	url(r'^otherPrivateStuff/$', views.main.otherPrivateStuff),
