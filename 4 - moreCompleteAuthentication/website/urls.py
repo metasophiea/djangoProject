9f5d52c6edf3a1f64ec3fd372d777bb14ed667bb
Brandon <metasophiea@gmail.com>
2017-12-14 13:57:07 +0000

    url work

--- 4 - moreCompleteAuthentication/website/urls.py
+++ 4 - moreCompleteAuthentication/website/urls.py
@@ -9,21 +9,37 @@ import newCode
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
+	
+	url(r'^signup/$', allauth.account.views.SignupView.as_view(), name='account_signup'),
+	url(r'^login/$', allauth.account.views.LoginView.as_view(), name='account_login'),
+	url(r'^logout/$', allauth.account.views.LogoutView.as_view(), name='account_logout'),
+	# (no 'as_view' - *name)
+	# AccountInactiveView				AddEmailForm					AjaxCapableProcessFormViewMixin
+	# ChangePasswordForm				CloseableSignupMixin			ConfirmEmailView
+	# EmailAddress						EmailConfirmation				EmailConfirmationHMAC
+	# EmailVerificationSentView			EmailView						FormView
+	# Http404							HttpResponsePermanentRedirect	HttpResponseRedirect
+	# INTERNAL_RESET_SESSION_KEY		INTERNAL_RESET_URL_KEY			ImmediateHttpResponse
+	# LoginForm							LoginView						LogoutView
+	# PasswordChangeView				PasswordResetDoneView			PasswordResetFromKeyDoneView
+	# PasswordResetFromKeyView			PasswordResetView				PasswordSetView
+	# RedirectAuthenticatedUserMixin	ResetPasswordForm				ResetPasswordKeyForm
+	# SetPasswordForm				   *SignupForm						SignupView
+	# TemplateResponseMixin				TemplateView					UserTokenForm
+	# View
 
-	url(r'^signup/$', lambda request: django.http.HttpResponseRedirect("/accounts/signup"), name='signup'),
-	url(r'^login/$', lambda request: django.http.HttpResponseRedirect("/accounts/login"), name='login'),
-	url(r'^logout/$', lambda request: django.http.HttpResponseRedirect("/accounts/logout"), name='logout'),
-
-    url(r'^accounts/', include('allauth.urls')),
+	url(r'^account/', include('allauth.urls')),
 	# /signup
 	# /login
 	# /logout
-	# /password/change
 	# /password/set
+	# /password/change
 	# /inactive
 	# /email
 	# /confirm-email
+	# /password/reset/
 	# /password/reset/done/
+	# /password/reset/key/
 	# /password/reset/key/done
 
 	url(r'^privateStuff/$', views.main.privateStuff),
