4205434f17c1c7a527167e8eec01a697134b8145
Brandon <metasophiea@gmail.com>
2017-12-15 16:11:19 +0000

    things

--- 4 - moreCompleteAuthentication/website/urls.py
+++ 4 - moreCompleteAuthentication/website/urls.py
@@ -9,14 +9,32 @@ import newCode
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
+
+
+
+
+	url(r'^account/', include('allauth.urls')),
+	# /signup
+	# /login
+	# /logout
+	# /password/set
+	# /password/change
+	# /inactive
+	# /email
+	# /confirm-email
+	# /password/reset/
+	# /password/reset/done/
+	# /password/reset/key/
+	# /password/reset/key/done
 	
 	url(r'^signup/$', allauth.account.views.SignupView.as_view(), name='account_signup'),
 	url(r'^login/$', allauth.account.views.LoginView.as_view(), name='account_login'),
 	url(r'^logout/$', allauth.account.views.LogoutView.as_view(), name='account_logout'),
-	url(r'^resetPassword/$', allauth.account.views.PasswordResetView.as_view(), name='account_password_reset'),
+	url(r'^resetPassword/$', allauth.account.views.PasswordResetView.as_view(), name='account_reset_password'),
 	url(r'^setPassword/$', allauth.account.views.PasswordChangeView.as_view(), name='account_password_change'),
 	url(r'^changePassword/$', allauth.account.views.PasswordChangeView.as_view(), name='account_password_change'),
-
+	url(r'^manageEmail/$', allauth.account.views.EmailView.as_view(), name='account_email'),
+	url(r'^manageEmail/$', allauth.account.views.ConfirmEmailView.as_view(), name='account_confirm_email'),
 	# (no 'as_view' - *name)
 	# AccountInactiveView				AddEmailForm					AjaxCapableProcessFormViewMixin
 	# ChangePasswordForm				CloseableSignupMixin			ConfirmEmailView
@@ -32,19 +50,8 @@ urlpatterns = [
 	# TemplateResponseMixin				TemplateView					UserTokenForm
 	# View
 
-	url(r'^account/', include('allauth.urls')),
-	# /signup
-	# /login
-	# /logout
-	# /password/set
-	# /password/change
-	# /inactive
-	# /email
-	# /confirm-email
-	# /password/reset/
-	# /password/reset/done/
-	# /password/reset/key/
-	# /password/reset/key/done
+
+
 
 	url(r'^privateStuff/$', views.main.privateStuff),
 	url(r'^otherPrivateStuff/$', views.main.otherPrivateStuff),
