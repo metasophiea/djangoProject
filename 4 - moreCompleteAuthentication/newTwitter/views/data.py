0371a273ad059a9a64ec87d1c59b34398ec67452
Brandon <metasophiea@gmail.com>
2017-12-18 17:16:40 +0000

    getting into queries

--- 4 - moreCompleteAuthentication/newTwitter/views/data.py
+++ 4 - moreCompleteAuthentication/newTwitter/views/data.py
@@ -1,7 +1,21 @@
 import django, json
 from .. import models
 
+def safeInt(val):
+	try:
+		return int(val)
+	except:
+		return None
+
+def getQuery(model,attribute,value):
+	try:
+		return  model.objects.get(**{attribute: value})
+	except:
+		return None
+
+
 def getTweets(request):
+	limit = 10
 	outputArray = []
 	defaultObject = {
 		'authorName':'',
@@ -10,12 +24,63 @@ def getTweets(request):
 		'downVotes':0
 	}
 
-	for item in models.tweet.tweet.objects.all():
-		tempObject = dict(defaultObject)
-		tempObject['authorName'] = item.author
-		tempObject['text'] = item.text
-		tempObject['upVotes'] = item.upVotes
-		tempObject['downVotes'] = item.downVotes
-		outputArray.append(tempObject)
+	index = safeInt(request.GET.get('index'))
+	startIndex = safeInt(request.GET.get('start'))
+	endIndex = safeInt(request.GET.get('end'))
+
+	if index is not None:
+		item = getQuery(models.tweet.tweet, 'tweetId', index)
+		if item is not None:
+			tempObject = dict(defaultObject)
+			tempObject['authorName'] = item.author
+			tempObject['text'] = item.text
+			tempObject['upVotes'] = item.upVotes
+			tempObject['downVotes'] = item.downVotes
+			outputArray.append(tempObject)
+		return django.http.HttpResponse( json.dumps(outputArray) )
+
+	if startIndex is None and endIndex is None:
+		# get all tweets
+		for item in models.tweet.tweet.objects.all():
+			tempObject = dict(defaultObject)
+			tempObject['authorName'] = item.author
+			tempObject['text'] = item.text
+			tempObject['upVotes'] = item.upVotes
+			tempObject['downVotes'] = item.downVotes
+			outputArray.append(tempObject)
+	elif startIndex is not None and endIndex is None:
+		# get the tweets after and including startIndex
+		items = models.tweet.tweet.objects.filter(tweetId__gte=startIndex, tweetId__lte=(startIndex+limit))
+		for item in items:
+			tempObject = dict(defaultObject)
+			tempObject['authorName'] = item.author
+			tempObject['text'] = item.text
+			tempObject['upVotes'] = item.upVotes
+			tempObject['downVotes'] = item.downVotes
+			outputArray.append(tempObject)
+
+	elif startIndex is None and endIndex is not None:
+		# get the tweets before and including endIndex 
+		items = models.tweet.tweet.objects.filter(tweetId__gte=(endIndex-limit), tweetId__lte=endIndex)
+		for item in items:
+			tempObject = dict(defaultObject)
+			tempObject['authorName'] = item.author
+			tempObject['text'] = item.text
+			tempObject['upVotes'] = item.upVotes
+			tempObject['downVotes'] = item.downVotes
+			outputArray.append(tempObject)
+	else:
+		# get the tweets between and including startIndex and endIndex 
+		if endIndex > (startIndex+limit):
+			endIndex = startIndex+limit
+		items = models.tweet.tweet.objects.filter(tweetId__gte=startIndex, tweetId__lte=endIndex)
+		for item in items:
+			tempObject = dict(defaultObject)
+			tempObject['authorName'] = item.author
+			tempObject['text'] = item.text
+			tempObject['upVotes'] = item.upVotes
+			tempObject['downVotes'] = item.downVotes
+			outputArray.append(tempObject)
+		pass
 
 	return django.http.HttpResponse( json.dumps(outputArray) )
--- 4 - moreCompleteAuthentication/website/urls.py
+++ 4 - moreCompleteAuthentication/website/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from newTwitter import views
 from django.views.generic.base import TemplateView
 import allauth.account.views
-import django
 
 import newCode
 
@@ -12,20 +11,21 @@ urlpatterns = [
 
 
 	# Authentication addresses
-	# url(r'^account/', include('allauth.urls')),
+	url(r'^account/', include('allauth.urls')), # still need to cover provider login
+		#
 	url(r'^signup/', allauth.account.views.signup, name='account_signup'),
 	url(r'^login/', allauth.account.views.login, name='account_login'),
 	url(r'^logout/', allauth.account.views.logout, name='account_logout'),
 	url(r"^inactive/$", allauth.account.views.account_inactive, name="account_inactive"),
 	url(r'^setPassword/', allauth.account.views.password_set, name='account_set_password'),
 	url(r'^changePassword/', allauth.account.views.password_change, name='account_change_password'),
-    url(r'^resetPassword/', allauth.account.views.password_reset, name='account_reset_password'),
+	url(r"^resetPassword/key/done/$", allauth.account.views.password_reset_from_key_done, name="account_reset_password_from_key_done"),
+	url(r"^resetPassword/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$", allauth.account.views.password_reset_from_key, name="account_reset_password_from_key"),
 	url(r"^resetPassword/done/$", allauth.account.views.password_reset_done, name="account_reset_password_done"),
-    url(r"^resetPassword/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$", allauth.account.views.password_reset_from_key, name="account_reset_password_from_key"),
-    url(r"^resetPassword/key/done/$", allauth.account.views.password_reset_from_key_done, name="account_reset_password_from_key_done"),
+    url(r'^resetPassword/', allauth.account.views.password_reset, name='account_reset_password'),
+	url(r"^confirmEmail/(?P<key>[-:\w]+)/$", allauth.account.views.confirm_email, name="account_confirm_email"),
 	url(r"^confirmEmail/$", allauth.account.views.email_verification_sent, name="account_email_verification_sent"),
-    url(r"^confirmEmail/(?P<key>[-:\w]+)/$", allauth.account.views.confirm_email, name="account_confirm_email"),
-	url(r'^manageEmail/', allauth.account.views.email, name='account_email'),
+    url(r'^manageEmail/', allauth.account.views.email, name='account_email'),
 
 
 	url(r'^privateStuff/$', views.main.privateStuff),
