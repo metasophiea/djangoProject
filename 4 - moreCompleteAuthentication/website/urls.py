4c9af9505096824e20e418b9ffd9c231dfb57147
Brandon <metasophiea@gmail.com>
2017-12-18 12:40:07 +0000

    update to the url patterns

--- 4 - moreCompleteAuthentication/website/urls.py
+++ 4 - moreCompleteAuthentication/website/urls.py
@@ -11,46 +11,21 @@ urlpatterns = [
 	url(r'^admin/', admin.site.urls),
 
 
-
-
-	url(r'^account/', include('allauth.urls')),
-	# /signup
-	# /login
-	# /logout
-	# /password/set
-	# /password/change
-	# /inactive
-	# /email
-	# /confirm-email
-	# /password/reset/
-	# /password/reset/done/
-	# /password/reset/key/
-	# /password/reset/key/done
-	
-	url(r'^signup/$', allauth.account.views.SignupView.as_view(), name='account_signup'),
-	url(r'^login/$', allauth.account.views.LoginView.as_view(), name='account_login'),
-	url(r'^logout/$', allauth.account.views.LogoutView.as_view(), name='account_logout'),
-	url(r'^resetPassword/$', allauth.account.views.PasswordResetView.as_view(), name='account_reset_password'),
-	url(r'^setPassword/$', allauth.account.views.PasswordChangeView.as_view(), name='account_password_change'),
-	url(r'^changePassword/$', allauth.account.views.PasswordChangeView.as_view(), name='account_password_change'),
-	url(r'^manageEmail/$', allauth.account.views.EmailView.as_view(), name='account_email'),
-	url(r'^manageEmail/$', allauth.account.views.ConfirmEmailView.as_view(), name='account_confirm_email'),
-	# (no 'as_view' - *name)
-	# AccountInactiveView				AddEmailForm					AjaxCapableProcessFormViewMixin
-	# ChangePasswordForm				CloseableSignupMixin			ConfirmEmailView
-	# EmailAddress						EmailConfirmation				EmailConfirmationHMAC
-	# EmailVerificationSentView			EmailView						FormView
-	# Http404							HttpResponsePermanentRedirect	HttpResponseRedirect
-	# INTERNAL_RESET_SESSION_KEY		INTERNAL_RESET_URL_KEY			ImmediateHttpResponse
-	# LoginForm							LoginView						LogoutView
-	# PasswordChangeView				PasswordResetDoneView			PasswordResetFromKeyDoneView
-	# PasswordResetFromKeyView			PasswordResetView				PasswordSetView
-	# RedirectAuthenticatedUserMixin	ResetPasswordForm				ResetPasswordKeyForm
-	# SetPasswordForm				   *SignupForm						SignupView
-	# TemplateResponseMixin				TemplateView					UserTokenForm
-	# View
-
-
+	# Authentication addresses
+	# url(r'^account/', include('allauth.urls')),
+	url(r'^signup/', allauth.account.views.signup, name='account_signup'),
+	url(r'^login/', allauth.account.views.login, name='account_login'),
+	url(r'^logout/', allauth.account.views.logout, name='account_logout'),
+	url(r"^inactive/$", allauth.account.views.account_inactive, name="account_inactive"),
+	url(r'^setPassword/', allauth.account.views.password_set, name='account_set_password'),
+	url(r'^changePassword/', allauth.account.views.password_change, name='account_change_password'),
+    url(r'^resetPassword/', allauth.account.views.password_reset, name='account_reset_password'),
+	url(r"^resetPassword/done/$", allauth.account.views.password_reset_done, name="account_reset_password_done"),
+    url(r"^resetPassword/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$", allauth.account.views.password_reset_from_key, name="account_reset_password_from_key"),
+    url(r"^resetPassword/key/done/$", allauth.account.views.password_reset_from_key_done, name="account_reset_password_from_key_done"),
+	url(r"^confirmEmail/$", allauth.account.views.email_verification_sent, name="account_email_verification_sent"),
+    url(r"^confirmEmail/(?P<key>[-:\w]+)/$", allauth.account.views.confirm_email, name="account_confirm_email"),
+	url(r'^manageEmail/', allauth.account.views.email, name='account_email'),
 
 
 	url(r'^privateStuff/$', views.main.privateStuff),
