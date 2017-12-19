7a942188fa3fce769ec6021a223cc5089901fb13
Brandon <metasophiea@gmail.com>
2017-12-19 14:30:49 +0000

    more of the templates redone

--- 4 - moreCompleteAuthentication/website/urls.py
+++ 4 - moreCompleteAuthentication/website/urls.py
@@ -13,19 +13,19 @@ urlpatterns = [
 	# Authentication addresses
 	url(r'^account/', include('allauth.urls')), # still need to cover provider login
 		#
-	url(r'^signup/', allauth.account.views.signup, name='account_signup'),
-	url(r'^login/', allauth.account.views.login, name='account_login'),
-	url(r'^logout/', allauth.account.views.logout, name='account_logout'),
-	url(r"^inactive/$", allauth.account.views.account_inactive, name="account_inactive"),
-	url(r'^setPassword/', allauth.account.views.password_set, name='account_set_password'),
-	url(r'^changePassword/', allauth.account.views.password_change, name='account_change_password'),
-	url(r"^resetPassword/key/done/$", allauth.account.views.password_reset_from_key_done, name="account_reset_password_from_key_done"),
-	url(r"^resetPassword/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$", allauth.account.views.password_reset_from_key, name="account_reset_password_from_key"),
-	url(r"^resetPassword/done/$", allauth.account.views.password_reset_done, name="account_reset_password_done"),
-    url(r'^resetPassword/', allauth.account.views.password_reset, name='account_reset_password'),
-	url(r"^confirmEmail/(?P<key>[-:\w]+)/$", allauth.account.views.confirm_email, name="account_confirm_email"),
-	url(r"^confirmEmail/$", allauth.account.views.email_verification_sent, name="account_email_verification_sent"),
-    url(r'^manageEmail/', allauth.account.views.email, name='account_email'),
+	url(r'^signup/', allauth.account.views.signup, name='account_signup'), #signup.html
+	url(r'^login/', allauth.account.views.login, name='account_login'), # login.html
+	url(r'^logout/', allauth.account.views.logout, name='account_logout'), # logout.html
+	url(r"^inactive/$", allauth.account.views.account_inactive, name="account_inactive"), # inactive.html
+	url(r'^setPassword/', allauth.account.views.password_set, name='account_set_password'), # password_set.html
+	url(r'^changePassword/', allauth.account.views.password_change, name='account_change_password'), # password_change.html
+	url(r"^resetPassword/key/done/$", allauth.account.views.password_reset_from_key_done, name="account_reset_password_from_key_done"), # password_reset_from_key_done.html
+	url(r"^resetPassword/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$", allauth.account.views.password_reset_from_key, name="account_reset_password_from_key"), # password_reset_from_key.html
+	url(r"^resetPassword/done/$", allauth.account.views.password_reset_done, name="account_reset_password_done"), # password_reset_done.html
+    url(r'^resetPassword/', allauth.account.views.password_reset, name='account_reset_password'), # password_reset.html
+	url(r"^confirmEmail/(?P<key>[-:\w]+)/$", allauth.account.views.confirm_email, name="account_confirm_email"), # email_confirm.html
+	url(r"^confirmEmail/$", allauth.account.views.email_verification_sent, name="account_email_verification_sent"), # verification_sent.html
+    url(r'^manageEmail/', allauth.account.views.email, name='account_email'), # email.html
 
 
 	url(r'^privateStuff/$', views.main.privateStuff),
